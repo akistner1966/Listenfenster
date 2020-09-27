b10cc63140eb6e6a56469a19a0b9c28dd4d87021
Andreas Kistner <akistner@t-online.de>
2020-09-27 13:20:01 +0200

    Liste definiert

--- Listenfenster/Listenfenster.py
+++ Listenfenster/Listenfenster.py
@@ -4,4 +4,35 @@ import tkinter as tk
 
 if __name__== "__main__":
     root = tk.Tk()
+    dlst = []
+    ele = ['A.xls', 260, '01.12.2001']
+    dlst.append(ele)
+    ele = ['B.xls', 1350, '01.12.2004']
+    dlst.append(ele)
+    ele = ['C.xls', 12260, '01.11.2001']
+    dlst.append(ele)
+    ele = ['A.txt', 260, '11.12.2011']
+    dlst.append(ele)
+    ele = ['B.txt', 1350, '10.12.2014']
+    dlst.append(ele)
+    ele = ['C.txt', 12260, '01.08.2011']
+    dlst.append(ele)
+    ele = ['A.jpg', 260, '01.01.2011']
+    dlst.append(ele)
+    ele = ['B.jpg', 1350, '01.07.2007']
+    dlst.append(ele)
+    ele = ['C.jpg', 12260, '01.04.2016']
+    dlst.append(ele)
+    ele = ['A.png', 260, '15.12.2011']
+    dlst.append(ele)
+    ele = ['B.png', 1350, '18.12.2014']
+    dlst.append(ele)
+    ele = ['C.png', 12260, '04.08.2011']
+    dlst.append(ele)
+    ele = ['A.doc', 260, '12.03.2020']
+    dlst.append(ele)
+    ele = ['B.doc', 1350, '18.08.2019']
+    dlst.append(ele)
+    ele = ['C.doc', 12260, '08.04.2010']
+    dlst.append(ele)
     root.mainloop()
