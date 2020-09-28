5db29c7e4c0b91d4d08ad326f3fda5e105d8580f
Andreas Kistner <akistner@t-online.de>
2020-09-28 18:48:29 +0200

    Mit korrekter Formatierung, aber noch nicht sortiert

--- Listenfenster/Listenfenster.py
+++ Listenfenster/Listenfenster.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
+import locale
 
 class listenfenster(object):
     def __init__(self, parent, headlne, fields, content):
@@ -18,55 +19,82 @@ class listenfenster(object):
         self.txtfeld.pack(side=tk.TOP)
         self.btnOK = tk.Button(self.frameDwn, text  ='OK', command=self.ok)
         self.btnOK.pack(side=tk.LEFT, padx=5, pady=5)
-        for datensatz in content: #Textfeld füllen
-            for ele in datensatz:
-                self.txtfeld.insert(tk.END, ele)
-            self.txtfeld.insert(tk.END, '\n')
-        
+        mxlen = [0]*4
+        for dsatz in content: #Max-Längen ermitteln - Teil 1
+            for cnt, ele in enumerate(dsatz, 0):
+                mxlen[cnt] = max(mxlen[cnt], len(ele))
+        for cnt, ele in enumerate(fields, 0): #Max-Längen ermitteln - Teil 2
+            mxlen[cnt] = max(mxlen[cnt], len(ele))
+        for cnt, ele in enumerate(fields, 0):
+            self.txthdln.insert(tk.END, ele)
+            if cnt < len(fields) - 1:
+                self.txthdln.insert(tk.END, ' ')
+        for dsatz in content: #Textfeld füllen
+            for cnt, ele in enumerate(dsatz, 0):
+                self.txtfeld.insert(tk.END, self._cuttrim(ele, cnt, mxlen[cnt]))
+                if cnt < len(dsatz) - 1:
+                    self.txtfeld.insert(tk.END, ' ')
+                else:
+                    self.txtfeld.insert(tk.END, '\n')
+
+    def _cuttrim(self, ein, ndx, mxlen):
+        if ndx != 2: #linksbündig
+            if len(ein) < mxlen:
+                ein += (mxlen - len(ein))*' '
+        else: #rechtsbündig
+            if len(ein) < mxlen:
+                ein = (mxlen - len(ein))*' ' + ein
+        return(ein)
+
     def ok(self):
         self.top.destroy()
+
+def mkstr(einlst):
+    einlst[2] = locale.format('%d', einlst[2], 1)
+    return(einlst)
 
 def mklst():
     dlst = []
     ele = ['A.xls', 'xls', 260, '01.12.2001']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['B.xls', 'xls', 1350, '01.12.2004']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['C.xls', 'xls', 12260, '01.11.2001']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['A.txt', 'txt', 260, '11.12.2011']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['B.txt', 'txt', 1350, '10.12.2014']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['C.txt', 'txt', 12260, '01.08.2011']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['A.jpg', 'jpg', 260, '01.01.2011']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['B.jpg', 'jpg', 1350, '01.07.2007']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['C.jpg', 'jpg', 12260, '01.04.2016']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['A.png', 'png', 260, '15.12.2011']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['B.png', 'png', 1350, '18.12.2014']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['C.png', 'png', 12260, '04.08.2011']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['A.doc', 'doc', 260, '12.03.2020']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['B.doc', 'doc', 21350, '18.08.2019']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     ele = ['C.doc', 'doc', 212260, '08.04.2010']
-    dlst.append(ele)
+    dlst.append(mkstr(ele))
     return(dlst)
 
 def mkfields():
-    return(['Dateiname', 'Extension', 'Dateigröße', 'Dateindatum'])
+    return(['Dateiname', 'Extension', 'Dateigröße', 'Dateidatum'])
 
 def showlist():
     lf = listenfenster(root, 'Demoliste', fliste, dliste)
 
 if __name__== "__main__":
+    locale.setlocale(locale.LC_NUMERIC, '')
     root = tk.Tk()
     dliste = mklst()
     fliste = mkfields()
